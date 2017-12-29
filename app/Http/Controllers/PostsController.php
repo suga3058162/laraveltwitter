3fcd96465ab7fc66dde3ee4bfd911021ed0c9664
Tomita Akane <tomita@itpropartners.jp>
2017-12-29 17:16:21 +0000

    Authによるユーザー認証を追加

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,16 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
     public function index(){
+
+      //Laravel入門 318ページ
+      $user = Auth::user();
+      // $sort = $request->sort;
+      // $items = Person::orderBy($sort, 'asc')
+      //     ->simplePagenate(5);
+      //Postを新しい順にDBから取得する
+      $posts = Post::latest()->get();
+      //配列に入れる
+      $param = ['user' => $user, 'posts' => $posts];
+
       // $posts = \App\Post::all();
       // $posts = Post::all();
       // $posts = Post::orderBy('created_at','desc')->get();
-      $posts = Post::latest()->get();
+      // $posts = Post::latest()->get();
       // dd($posts->toArray());
-      return view('posts.index', ['posts' => $posts]);
+      return view('posts.index', $param);
       // return view('posts.index')->with('posts', $posts);
     }
 
