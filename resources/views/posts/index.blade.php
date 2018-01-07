e1519409e61950160b77cde25fae2e9a393f64a2
Tomita Akane <tomita@itpropartners.jp>
2018-01-07 02:35:01 +0000

    /post 一覧画面のhtml,css修正

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,13 +9,14 @@ Blog Posts
 
 @section('content')
 @if (Auth::check())
-<p>USER: {{ $user->name . '(' . $user->email . ')' }}</p>
+<p class="login_user_name">ログインユーザー名: {{ $user->name . '(' . $user->email . ')' }}</p>
+<p class="login_user_id">ログインユーザーID: {{ $user->id }}</p>
 @else
 <p>※ログインしていません。(<a href="/login">ログイン</a>|<a href="/register">登録</a>)</p>
 @endif
 <h1>
-  <a href="{{ url('/post/create')}}" class="header-menu">New Post</a>
-  Blog Posts
+  <a href="{{ url('/post/create')}}" class="header-menu">ツイートする</a>
+  ツイート一覧
 </h1>
 <ul>
   {{--
@@ -25,16 +26,23 @@ Blog Posts
   --}}
   @forelse ($posts as $post)
   <li>
-    <p>投稿ユーザー：{{ $post->user_id }}</p>
-    <a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a>
+    <p class="user">投稿ユーザーID：{{ $post->user_id }}</p>
+
+    <div class="list_post_wrap">
+    <h3>ツイートタイトル：</h3>
+    <p class="post_title">{{ $post->title }}</p>
+    <h3>ツイート内容：</h3>
+    <p class="post_body">{{ $post->body }}</p>
+    </div>
 
     {!! Form::open(['method' => 'post','url' => 'likes']) !!}
     <input type="hidden" name="post_id" value="{{ $post->id }}">
     <button type="submit">Like</button>
     {!! Form::close() !!}
 
-    <a href="{{ action('PostsController@edit', $post) }}" class="edit">[Edit]</a>
-    <a href="#" class="del" data-id="{{ $post->id }}">[x]</a>
+    <a href="{{ action('PostsController@show', $post) }}" class="post_detail_link">[ツイート詳細画面へ]</a>
+    <a href="{{ action('PostsController@edit', $post) }}" class="edit">[ツイート編集画面へ]</a>
+    <a href="#" class="del" data-id="{{ $post->id }}">[ツイート削除]</a>
     <form method="post" action="{{ url('/post', $post->id) }}" id="form_{{ $post->id }}">
     {{ csrf_field() }}
     {{ method_field('delete') }}
