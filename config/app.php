82dc9da909f741eb63ba7131bb148239b93ea160
tomita <sugawara.an@gmail.com>
2018-01-08 16:24:34 +0900

    clockwork導入

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Clockwork\Support\Laravel\ClockworkServiceProvider::class,
 
         /*
          * Package Service Providers...
