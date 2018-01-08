f2abbd998addfaacac1d7c5324ffdc0e9a22baf1
tomita <sugawara.an@gmail.com>
2018-01-08 20:05:31 +0900

    リツイートを/postのツイート一覧に表示させる

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 use App\User;
+use App\Retweet;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\PostRequest;
 
@@ -12,12 +13,22 @@ class PostsController extends Controller
 {
     public function index(){
       $user = Auth::user();
+
+      //自分がフォローしているユーザーのツイート
+      //自分のツイート
       //フォローしているユーザーIDを取得する
       $userIds = $user->follows->pluck('id');
-      $userIds[] = $user->id;
+
+      //自分がフォローしているユーザーのリツイート
+      //自分のリツイート
+      // $postIds = $posts->retweets->pluck('post_id');
+      // $postIds[] = $posts->$user->id;
+
+      //特定のuser_idに紐づくretweetsテーブルに入っているpost_idを取得する
+      $postIds = Retweet::whereIn('user_id', $userIds)->orWhere('user_id',$user->id)->pluck('post_id');
 
       //フォローしているユーザーIDを元に、フォローしているユーザーのツイートを、タイムスタンプが新しい順に取得する
-      $posts = Post::whereIn('user_id', $userIds)->latest()->get();
+      $posts = Post::whereIn('user_id', $userIds)->orWhere('user_id',$user->id)->orWhereIn('id', $postIds)->latest()->get();
 
       //Postを新しい順にDBから取得する
       // $posts = Post::latest()->get();
