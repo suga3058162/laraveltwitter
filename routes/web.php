c3a4e5ee080c8634be5720f9142ec21cda99c458
tomita <sugawara.an@gmail.com>
2018-01-10 20:49:05 +0900

    ルーティング修正

--- routes/web.php
+++ routes/web.php
@@ -51,4 +51,4 @@ Route::delete('/likes/{id}', 'LikesController@destroy');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-// Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
