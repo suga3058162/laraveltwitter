0cc3fa35784494c08ec696be7089913202aff350
tomita <sugawara.an@gmail.com>
2018-01-10 21:04:10 +0900

    コメントアウトしていた不要なコードを削除、解説追加

--- app/Http/Controllers/LikesController.php
+++ app/Http/Controllers/LikesController.php
@@ -15,73 +15,39 @@ class LikesController extends Controller
     protected $request;
 
     public function __construct(Request $request) {
-    //   $this->request = $request;
+      // Likeモデルを取得
       $this->LikeModel = \App::make('\App\Like');
     }
 
     public function index($id){
-      // $user = Auth::user();
-      // //特定のuser_idを取得
-      // $userId = Auth::user()->id;
-      //userを検索
+      // ユーザーidを取得
       $user = User::where('id', $id)->first();
-      //特定のuser_idに紐づくlikesテーブルに入っているpost_idを取得する
+      // 特定のuser_idに紐づくlikesテーブルに入っているpost_idを取得する
       $postIds = Like::where('user_id', $user->id)->latest()->get();
-      //postを取得
+      // ポストidを取得
       $posts = Post::whereIn('id', $postIds)->get();
-      //配列に入れる
+      // ユーザーidとポストidを配列に入れる
       $param = ['user' => $user,'posts' => $posts];
       return view('likes.index', $param);
     }
 
     public function store(Request $request) {
-      //
-      // $this->validate($request,[
-      //   'id' => 'required'
-      // ]);
-      // $like = new Like(['id' => $request->id]);
-      // $post->likes()->save($like);
-      // return redirect()->action('PostsController@show', $post);
-
-      // $postData = $this->request->all();
-      // dd($postData);
-
-      //mayer-consumer UsersController 266行目 profileDetailEditPostファンクションを参考
-      // dd($post);
-      //
-      // $Like = $this->LikeModel->firstOrcreate(['post_id' => $post->id]);
-      // $Like->fill($postData['Like']);
-      // $Like->save();
-
-      //PostsController 44行　storeファンクション参考
-      // dd($request);
-      // $post = new Post();
-
-      //mayer-consumer UsersController profileDetailEditPost参考
-      // $like = $this->LikeModel->where('post', $post->id)->get();
-      // dd($like);
-
+      //likeボタンを押下したツイートidを取得
       $post_id = Input::get('post_id');
-
+      //ログインユーザーidを取得
       $user = \Auth::user();
-
-
-      // $post_id->save();
+      //ツイートidとユーザーidをlikeテーブルへ保存
       $this->LikeModel->create(['post_id' => $post_id,'user_id' => $user->id]);
       return redirect('/post');
-
     }
 
     public function destroy($id) {
-      // dd(Follow::where('to_user_id', $id)->get());
-      // $follow = Follow::where('to_user_id',$id)->where('from_user_id',Auth::user()->id)->first();
-      // dd(Like::where('post_id',$id)->where('user_id',Auth::user()->id)->first());
+      // viewでいいね解除したツイートidと、likesテーブルのpost_idが一致するレコードを取得
+      // ログインユーザーidと、likesテーブルのuser_idが一致するレコードを取得
       $like = Like::where('post_id',$id)->where('user_id',Auth::user()->id)->first();
-      // $follow = Follow::where('from_user_id',$id)->where('to_user_id',Auth::user()->id)->first();
-      // dd(Auth::user()->id);
-      // $follow->delete();
+      // 一致したレコードを削除
       $like->delete();
-      // return redirect("/user/{{$id}}");
+      // /postヘリダイレクト
       return redirect("/post");
     }
 }
