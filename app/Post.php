0e571a05d03aad71b3af81e657fe67a955c4ce88
tomita <sugawara.an@gmail.com>
2018-01-10 21:56:27 +0900

    コメントアウトしていた不要なコードを削除、インデント整理

--- app/Post.php
+++ app/Post.php
@@ -8,35 +8,25 @@ use App\Like;
 
 class Post extends Model
 {
-    //
-
     protected $fillable = ['user_id','title', 'body'];
 
     public function comments() {
       return $this->hasMany('App\Comment');
     }
 
-    //いいね機能追加
-    // public function comments() {
-    //   return $this->hasMany('App\Comment');
-    // }
-
-    public function user()
-    {
+    public function user() {
       return $this->belongsTo(User::class);
     }
 
-    public function likes()
-    {
+    public function likes() {
       return $this->hasMany('App\Like');
     }
 
-    public function like_by()
-    {
+    public function like_by() {
       return Like::where('user_id', Auth::user()->id)->first();
     }
 
-    public function isLiked(){
+    public function isLiked() {
       $like = Like::where('post_id', $this->id)->where('user_id', Auth::user()->id)->first();
       logger(isset($like));
       return isset($like);
