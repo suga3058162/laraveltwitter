06b3bf6c50e09af509f6adaf80ff73954f27bac0
tomita <sugawara.an@gmail.com>
2018-01-10 22:05:02 +0900

    コメントアウト修正、不要なコードを削除

--- app/User.php
+++ app/User.php
@@ -29,22 +29,17 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    /**
-     * Postを取得
-     */
-    public function posts(){
+    // Postを取得
+    public function posts() {
         return $this->hasMany('App\Post', 'user_id');
     }
-    /**
-     * followしているユーザーidを取得
-     */
-    public function follows(){
+    // followしているユーザーidを取得
+    public function follows() {
         return $this->hasMany('App\Follow', 'from_user_id');
-      }
+    }
 
-    //いいね機能追加
-    public function likes()
-    {
+    // likesテーブルのリレーション
+    public function likes() {
       return $this->hasMany(Like::class);
     }
 
@@ -53,46 +48,8 @@ class User extends Authenticatable
     }
 
     public function isFollowd(){
-        // $follow = Follow::where('from_user_id', $this->id)->where('to_user_id', Auth::user()->id)->first();
-        // $follow = Follow::where('from_user_id', $this->id)->first();
-        
-        // 画面のユーザーid
-        // dd($this->id);
-
-        // ログインユーザーid
-        // dd(Auth::user()->id);
-
-        // ログインユーザーがフォローしているユーザーid（没）
-        // $follow = Follow::where('from_user_id', $this->id)->where('to_user_id', Auth::user()->id)->get();
-        // $follow = Follow::where('to_user_id', Auth::user()->id)->get();
-        // dd($follow['from_user_id']);
-    
-        // $follow = Follow::where('from_user_id', Auth::user()->id)->get();
-
-        // ログインユーザーがフォローしているユーザーid（ok）
-        // $follow = Follow::where('from_user_id', Auth::user()->id)->pluck('to_user_id');
-        // dd($follow); 
-        //user_id 1 の場合、arrayで[1,2,3,3,3,3]が入っている
-
-        // ログインユーザーがフォローしているユーザーidに、画面のユーザーidが含まれていたらtrue、含まれていなかったらfalse
-        // return boolval($follow === $this->id);
-
-        // 自分自身をフォローしているか判定
-        // $serch = 1;
-        // $key = in_array($serch,$follow);
-
-        // logger(isset($follow));
-        // return isset($follow);
-
-        // $follow = Follow::where('from_user_id', $this->id)->where('to_user_id', Auth::user()->id)->first();
-
         $follow = Follow::where('to_user_id', $this->id)->where('from_user_id', Auth::user()->id)->first();
-
         logger(isset($follow));
         return isset($follow);
-        // dd(isset($follow));
-        // $split = array_splice($follow, 1); //特定のidを削除（失敗）
-        // dd($split);
-        // dd(isset($split));
     }
 }
