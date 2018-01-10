ea7f295e772a2f4149d58c0a9d67f303dc8391dd
tomita <sugawara.an@gmail.com>
2018-01-10 21:54:30 +0900

    コメントアウトしていた不要なコードを削除

--- app/Comment.php
+++ app/Comment.php
@@ -6,10 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
     protected $fillable = ['body'];
 
-    // $commet->post
     public function post() {
       return $this->belongsTo('App\Post');
     }
